e7202025dbe127b5c3774bf7fc61f9308f1fb456
guniism <gun16110@hotmail.com>
2025-03-05 02:35:46 +0700

    guniism's Bar

--- src/ui/Bar.java
+++ src/ui/Bar.java
@@ -4,32 +4,55 @@ import game.GameController;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import utils.SpriteSheet;
 
-public class Bar extends Canvas {
-    private static final int TILE_WIDTH = 34;
-    private static final int TILE_HEIGHT = 8;
-    private static final int SCALE = GameController.getScale();
+public class Bar extends Pane {
+	private static final int TILE_WIDTH = 34;
+	private static final int TILE_HEIGHT = 8;
+	private static final int SCALE = GameController.getScale();
+	private Canvas innerBar;
+	private GraphicsContext innerGC;
+	private int maxValue;
+	private double srcX;
+	private double srcY;
 
-    private Image spriteSheet;
+	public Bar(int maxValue, int type) {
+		this.maxValue = maxValue;
+		this.srcX = (type == 1) ? 6 : 36;
+		this.srcY = 8;
+		
+		SpriteSheet bgSprite = new SpriteSheet("bar-sprite.png", 34, 8, 0, 34, 1);
+		SpriteSheet frameSprite = new SpriteSheet("bar-sprite.png", 34, 8, 0, 0, 1);
+		this.getChildren().add(bgSprite);
+		this.setWidth(TILE_WIDTH * SCALE);
+		this.setHeight(TILE_HEIGHT * SCALE);
 
-    public Bar() {
-        String path = ClassLoader.getSystemResource("bar-sprite.png").toString();
-        spriteSheet = new Image(path);
+		innerBar = new Canvas();
+		innerGC = innerBar.getGraphicsContext2D();
+		innerBar.setLayoutX(2 * GameController.getScale());
+		innerBar.setLayoutY(2 * GameController.getScale());
+		innerGC.scale(GameController.getScale(), GameController.getScale());
+		innerGC.setImageSmoothing(false);
+		setBar(this.maxValue);
 
-        this.setWidth(TILE_WIDTH * SCALE);
-        this.setHeight(TILE_HEIGHT * SCALE);
+		this.getChildren().add(innerBar);
+		this.getChildren().add(frameSprite);
+	}
 
-        drawBar();
-    }
+	public void setBar(int value) {
+		int tileWidth = 30;
+		int tileHeight = 4;
 
-    private void drawBar() {
-        GraphicsContext gc = this.getGraphicsContext2D();
-        gc.setImageSmoothing(false);
-        gc.clearRect(0, 0, this.getWidth(), this.getHeight());
+		String path = ClassLoader.getSystemResource("bar-sprite.png").toString();
+		Image spriteSheet = new Image(path);
+		innerBar.setWidth(tileWidth * GameController.getScale());
+		innerBar.setHeight(tileHeight * GameController.getScale());
 
-        double srcX = 0;
-        double srcY = 0;
+		innerGC.clearRect(0, 0, this.getWidth(), this.getHeight());
 
-        gc.drawImage(spriteSheet, srcX, srcY, TILE_WIDTH, TILE_HEIGHT, 0, 0, TILE_WIDTH * SCALE, TILE_HEIGHT * SCALE);
-    }
+		double destX = -(tileWidth - (tileWidth * (double) value / this.maxValue));
+		double destY = 0;
+		innerGC.drawImage(spriteSheet, this.srcX, this.srcY, tileWidth, tileHeight, destX, destY, tileWidth, tileHeight);
+	}
 }
